bc0626192ed69e9f9f5ef5b60c2d0e380035a1ef
Levan D <97022558+Levan-D@users.noreply.github.com>
2026-08-07 11:42:30 +0400

    Update LandingPage.tsx

--- src/pages/_admin/LandingPage.tsx
+++ src/pages/_admin/LandingPage.tsx
@@ -3,7 +3,7 @@ import { SecondScreen } from '@/features/_admin/landing/components/SecondScreen'
 import { FiltersConfig } from '@/features/_admin/landing/components/FiltersConfig';
 import { ModelsOnLanding } from '@/features/_admin/landing/components/ModelsOnLanding';
 import { LivePreview } from '@/features/_admin/landing/components/LivePreview';
-
+//adasda
 export default function LandingPage() {
   return (
     <div className="flex gap-3 @max-wide:flex-col @mobile:gap-4">
